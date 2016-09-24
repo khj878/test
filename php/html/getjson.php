25c19a5a01a9160d6f0ca50135389bbd3e230ec8
heejun_kim <khj878@hanmail.net>
2016-09-24 20:35:47 +0900

    Add files via upload

---
+++ php/html/getjson.php
@@ -0,0 +1,47 @@
+﻿<?php 
+	header('Content-Type: text/html; charset=UTF-8');
+	$userid = urldecode($_POST['userid']);
+	$password = urldecode($_POST['password']);
+	
+	$mysql_hostname = 'localhost';
+	$mysql_username = 'root';
+	$mysql_password = 'gmlwns12';
+	$mysql_database = 'petmonitor';
+	$mysql_port = '3306';
+	$mysql_charset = 'utf8';
+	mysql_query(' SET NAMES '.$mysql_charset);
+
+	$connect = @mysql_connect($mysql_hostname.':'.$mysql_port, $mysql_username, $mysql_password); 
+	@mysql_select_db($mysql_database, $connect) or die('DB 선택 실패');
+	
+	// $query = "select * from test where userid = ".$data ;
+	$query = "select * from test where userid = '".$userid."'"." and password = '".$password."'" ;
+	//echo $query;
+	$result = mysql_query($query);
+	$jsonData      = array();
+	$jsonTempData  = array();
+	
+	while($row = mysql_fetch_array($result)){
+		$jsonTempData['userid'] = $row[userid];
+		$jsonTempData['password'] = $row[password];
+		$jsonTempData['auto_tmp'] = $row[auto_tmp];
+		$jsonTempData['auto_feed'] = $row[auto_feed];
+		$jsonTempData['min_tmp'] = $row[min_tmp];
+		$jsonTempData['max_tmp'] = $row[max_tmp];
+		$jsonTempData['cur_tmp'] = $row[cur_tmp];
+		$jsonTempData['feed_btn'] = $row[feed_btn];
+		$jsonTempData['fan_btn'] = $row[fan_btn];
+		$jsonTempData['heatpad_btn'] = $row[heatpad_btn];
+		$jsonTempData['set_feed_time1'] = $row[set_feed_time1];
+		$jsonTempData['set_feed_time2'] = $row[set_feed_time2];
+		$jsonTempData['set_feed_time3'] = $row[set_feed_time3];
+		$jsonData[] = $jsonTempData;
+	} 
+	
+	$outputArr = array();
+	$outputArr['result'] = $jsonData;
+	 
+	print_r(json_encode($outputArr));
+
+	mysql_close($connect);
+ ?>
---
+++ php/html/setting.php
@@ -0,0 +1,62 @@
+﻿<?php 
+	header('Content-Type: text/html; charset=UTF-8');
+	$mode_flag = urldecode($_POST['mode_flag']);
+	$userid = urldecode($_POST['userid']);
+	$password = urldecode($_POST['password']);
+	$auto_tmp = urldecode($_POST['auto_tmp']);
+	$auto_feed = urldecode($_POST['auto_feed']);
+	$min_tmp = urldecode($_POST['min_tmp']);
+	$max_tmp = urldecode($_POST['max_tmp']);
+	$btn_fan = urldecode($_POST['btn_fan']);
+	$btn_heatpad = urldecode($_POST['btn_heatpad']);
+	$btn_feed = urldecode($_POST['btn_feed']);
+	$set_feed_time1 = urldecode($_POST['set_feed_time1']);
+	$set_feed_time2 = urldecode($_POST['set_feed_time2']);
+	$set_feed_time3 = urldecode($_POST['set_feed_time3']);
+	
+	$mysql_hostname = 'localhost';
+	$mysql_username = 'root';
+	$mysql_password = 'gmlwns12';
+	$mysql_database = 'petmonitor';
+	$mysql_port = '3306';
+	$mysql_charset = 'utf8';
+	mysql_query(' SET NAMES '.$mysql_charset);
+
+	$connect = @mysql_connect($mysql_hostname.':'.$mysql_port, $mysql_username, $mysql_password); 
+	@mysql_select_db($mysql_database, $connect) or die('DB 선택 실패');
+	
+	if(strcmp($mode_flag, "1") == 0) {
+		$query = "update test set auto_tmp = ".$auto_tmp.";";
+		$result = mysql_query($query);
+		$query = "update test set auto_feed = ".$auto_feed.";";
+		$result = mysql_query($query);
+		
+		if(strlen($min_tmp) != 0) {	
+			$query = "update test set min_tmp = ".$min_tmp.";";
+			$result = mysql_query($query);}
+		if(strlen($max_tmp) != 0) {	
+			$query = "update test set max_tmp = ".$max_tmp.";";
+			$result = mysql_query($query);}
+		
+		if(strlen($set_feed_time1) != 0) {$query = "update test set set_feed_time1 = '".$set_feed_time1."';";
+		$result = mysql_query($query);}
+		if(strlen($set_feed_time2) != 0) {$query = "update test set set_feed_time2 = '".$set_feed_time2."';";
+		$result = mysql_query($query);}
+		if(strlen($set_feed_time3) != 0) {$query = "update test set set_feed_time3 = '".$set_feed_time3."';";
+		$result = mysql_query($query);}
+	}
+	
+	else if(strcmp($mode_flag, "2") == 0) {
+		$query = "update test set fan_btn = ".$btn_fan.";";
+		$result = mysql_query($query);
+	}
+	else if(strcmp($mode_flag, "3") == 0) {
+		$query = "update test set heatpad_btn = ".$btn_heatpad.";";
+		$result = mysql_query($query);
+	}
+	else if(strcmp($mode_flag, "4") == 0) {
+		$query = "update test set feed_btn = ".$btn_feed.";";
+		$result = mysql_query($query);
+	}
+	mysql_close($connect);
+ ?>
